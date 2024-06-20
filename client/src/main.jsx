890143db01f88ac9b375c8b58aa10718e5364f96
gerciocrystal <gerciobandep@gmail.com>
2024-06-20 21:33:34 +0200

    save sensive data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,23 @@ import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import UserProvider from "./context/UserProvider";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { render, screen } from "@testing-library/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <Router>
-    <UserProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </UserProvider>
-  </Router>
+  test("Render UP", () => {
+    render(
+      <Router>
+        <UserProvider>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </UserProvider>
+      </Router>
+    );
+    const linkElement = screen.getByText(/Saiba Mais/i);
+
+    expect(linkElement).toBeInTheDocument;
+  })
+
   // </React.StrictMode>
 );
